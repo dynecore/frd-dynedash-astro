434e61fcf7db8687046b5b0c35351049f2581cc3
Aira <aira@dynecore.com>
2024-11-26 18:53:01 -0300

    ws removed, session cookie added to status site, strange type related fake-errors fixed

--- public/jscode.js
+++ public/jscode.js
@@ -1,32 +1,31 @@
 export const webcontroller = (funcion, interval, timeout) => {
-
-    console.log({interval, timeout})
-    const thisInterval = setInterval(() => {
-        funcion()
-    }, interval * 1000);
-    
+  console.log({ interval, timeout })
+  const thisInterval = setInterval(() => {
     funcion()
+  }, interval * 1000)
 
-    setTimeout(() => {
-        clearInterval(thisInterval)
-        console.log('finishing interval')
-        location.reload()
-    }, timeout);
+  funcion()
 
+  setTimeout(() => {
+    clearInterval(thisInterval)
+    console.log('finishing interval')
+    // eslint-disable-next-line no-undef
+    location.reload()
+  }, timeout)
 }
 
 export const expCounter = (funcion, expireIn) => {
-    //let expirationCounter = expireIn
+  // let expirationCounter = expireIn
 
-    const thisInterval = setInterval(() => {
-        funcion()
-        //expirationCounter - 1000
-    }, 1000);
-    
+  const thisInterval = setInterval(() => {
     funcion()
+    // expirationCounter - 1000
+  }, 1000)
 
-    setTimeout(() => {
-        clearInterval(thisInterval)
-        console.log('finishing interval')
-    }, expireIn * 1000);
+  funcion()
+
+  setTimeout(() => {
+    clearInterval(thisInterval)
+    console.log('finishing interval')
+  }, expireIn * 1000)
 }
